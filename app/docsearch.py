96c999500a3c57b0784656f35c1d1da9a64d44d9
Muhammed Ashiq Abdul Khader <ashiqabdulkhader@gmail.com>
2022-03-22 13:32:45 +0530

    ranker added

--- app/docsearch.py
+++ app/docsearch.py
@@ -21,4 +21,5 @@ def query(question):
     q_da = DocumentArray([Document(text=question)])
     encoder.encode(docs=q_da)
     indexer.search(docs=q_da)
+    ranker.rank(docs=q_da)
     return q_da[0].matches[:1][0].text
--- app/test.py
+++ app/test.py
@@ -2,6 +2,6 @@ import requests
 import json
 
 url = "http://127.0.0.1:8000/search"
-payload = {'id': '1', 'lang': 'en', 'query': 'who invented python'}
+payload = {'id': '1', 'lang': 'en', 'query': 'when was python created?'}
 r = requests.post(url, data=json.dumps(payload))
 print(r.text)
